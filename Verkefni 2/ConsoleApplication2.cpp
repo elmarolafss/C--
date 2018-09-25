8a1df8b0facb81a8d0d5745451e3aa5325c2f6c0
elmarolafss <elmarol250@nemi.tskoli.is>
2018-09-25 02:37:38 +0000

    agh

--- Verkefni 2/ConsoleApplication2.cpp
+++ Verkefni 2/ConsoleApplication2.cpp
@@ -17,16 +17,6 @@ unsigned int whole(int *originArray) {
 	return ip_long;
 }
 
-int print_ip(int ip)
-{
-	unsigned char bytes[4];
-	bytes[0] = ip & 0xFF;
-	bytes[1] = (ip >> 8) & 0xFF;
-	bytes[2] = (ip >> 16) & 0xFF;
-	bytes[3] = (ip >> 24) & 0xFF;
-	printf("%d.%d.%d.%d\n", bytes[3], bytes[2], bytes[1], bytes[0]);
-}
-
 int main()
 {
 	cout << "Sláğu inn upphafsnet: ";
