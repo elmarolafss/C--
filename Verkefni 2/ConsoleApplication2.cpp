fa52be92090c1db8ea8ebf85c491d411dfcd9e09
elmarolafss <elmarol250@nemi.tskoli.is>
2018-09-25 17:20:45 +0000

    done

--- Verkefni 2/ConsoleApplication2.cpp
+++ Verkefni 2/ConsoleApplication2.cpp
@@ -12,9 +12,24 @@ void printarray(int arg[], int length) {
 		cout << arg[n] << ' ';
 	cout << '\n';
 }
-unsigned int whole(int *originArray) {
+
+unsigned int whole(int *originArray, int boolean, int magic) {
 	unsigned int ip_long = (originArray[0] << 24) | (originArray[1] << 16) | (originArray[2] << 8) | originArray[3];
-	return ip_long;
+	if (boolean == 0){
+		return ip_long;
+	};
+	if (boolean == 1){
+		return ip_long + magic;
+	};
+}
+
+
+void backToIp(int *newArray, int ipWhole){
+	newArray[0] = (ipWhole >> 24) & 0xff;
+	newArray[1] = (ipWhole >> 16) & 0xff;
+	newArray[2] = (ipWhole >> 8) & 0xff;
+	newArray[3] = ipWhole & 0xff;
+	cout << "|" << newArray[0] << "." << newArray[1] << "." << newArray[2] << "." << newArray[3] << "|    ";
 }
 
 int main()
@@ -26,11 +41,33 @@ int main()
 	cout << "Fjöldi neta sem á að skipta upphafsnetinu í: ";
 	cin >> netNum;
 	int originArray[4] = { originA, originB, originC, originD };
-
-	double bitNum = log2(netNum);
+	int newArray[4];
+	int bitNum = log2(netNum);
 	int power = 32 - (originBits + bitNum);
 	int magicNumber = pow(2, power);
-	cout << print_ip(3232235776);
-    return 0;
+	int magicNumber2 = magicNumber;
+	int i = 0;
+	unsigned int tempnet = 0xffffffff << (32 - (originBits + bitNum));
+	cout << tempnet << endl;
+	cout << "IP net        Fyrsta nothæfa       Síðasta nothæfa        brodcast         Maski" << endl;
+	cout << "|---------------------------------------------------------------------------------|" << endl;
+	backToIp(newArray, whole(originArray, 0, magicNumber2));
+	backToIp(newArray, whole(originArray, 0, magicNumber2) + 1);
+	backToIp(newArray, whole(originArray, 0, magicNumber2) + magicNumber - 2);
+	backToIp(newArray, whole(originArray, 0, magicNumber2) + magicNumber - 1);
+	backToIp(newArray, tempnet);
+	cout << endl;
+
+	for (i; i < netNum - 1; i++){
+		backToIp(newArray, whole(originArray, 1, magicNumber2));
+		backToIp(newArray, whole(originArray, 1, magicNumber2) + 1);
+		backToIp(newArray, whole(originArray, 1, magicNumber2) + magicNumber - 2);
+		backToIp(newArray, whole(originArray, 1, magicNumber2) + magicNumber - 1);
+		backToIp(newArray, tempnet);
+		cout << endl;
+		magicNumber2 += magicNumber;
+	}
+
+	return 0;
 }
 
